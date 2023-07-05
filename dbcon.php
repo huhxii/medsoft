46cbf82e0742e2969f60d8f7410c9af4760a4e77
91hjm <91hjm@naver.com>
2023-07-05 16:40:12 +0900

    dbcon.php modify

--- dbcon.php
+++ dbcon.php
@@ -1,11 +1,23 @@
 <?php
-$serverName = 'localhost'; // MySQL 서버 이름
-$userName = 'root'; // MySQL 사용자 이름
-$password = '123456'; // MySQL 사용자 비밀번호
-$dbName = 'test_board'; // 사용할 데이터베이스 이름
+if($_SERVER['REMOTE_ADDR'] == '121.136.238.5'){
+    $serverName = 'localhost'; // MySQL 서버 이름
+    $userName = 'root'; // MySQL 사용자 이름
+    $password = '5263'; // MySQL 사용자 비밀번호
+    $dbName = 'test_board'; // 사용할 데이터베이스 이름
 
-$mysqli = new mysqli($serverName, $userName, $password, $dbName);
-if ($mysqli->connect_errno) {
-    die("failed to connect mysql => ".$mysqli->connect_error);
+    $mysqli = new mysqli($serverName, $userName, $password, $dbName);
+    if ($mysqli->connect_errno) {
+        die("failed to connect mysql => ".$mysqli->connect_error);
+    }
+}else{
+    $serverName = 'localhost'; // MySQL 서버 이름
+    $userName = 'root'; // MySQL 사용자 이름
+    $password = '123456'; // MySQL 사용자 비밀번호
+    $dbName = 'test_board'; // 사용할 데이터베이스 이름
+
+    $mysqli = new mysqli($serverName, $userName, $password, $dbName);
+    if ($mysqli->connect_errno) {
+        die("failed to connect mysql => ".$mysqli->connect_error);
+    }
 }
 ?>
